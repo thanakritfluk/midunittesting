bec42cc22d41f6c98efec253417cd525456a0b65
Pakanon Pantisawat <pakanon.pk@gmail.com>
2018-09-29 19:13:31 +0700

    Unittest

--- java/StringUtil.java
+++ java/StringUtil.java
@@ -1,9 +1,21 @@
 
 public class StringUtil {
+    /**
+     * Concat two string together.
+     * 
+     * @param a String to concat
+     * @param b String to concat
+     * @return concaternated result of a and b
+     */
     public static String concat(String a, String b) {
         return a + b;
     }
 
+    /**
+     * Check for duplicate character in string and remove
+     * 
+     * @return string with no duplicate character
+     */
     public static String remove_duplicate(String a) {
         for (int i = 0; i < a.length(); i++) {
             if (a.substring(i).equals(a.substring(i + 1))) {
@@ -15,6 +27,11 @@ public class StringUtil {
         return a;
     }
 
+    /**
+     * Duplicate string for specified round
+     * 
+     * @return string with specified round
+     */
     public static String duplicate(String a, int round) {
         String result = "";
         for (int i = 0; i < round; i++) {
@@ -24,6 +41,13 @@ public class StringUtil {
         return result;
     }
 
+    /**
+     * Find character in string and return index of a
+     * 
+     * @param a String to find character
+     * @param c character to find
+     * @return index of character -1 if not found
+     */
     public static int find(String a, char c) {
         for (int i = 0; i < a.length(); i++) {
             if (a.charAt(i) == (c)) {
--- python/string_util.py
+++ python/string_util.py
@@ -2,6 +2,7 @@ def concat(a, b):
     """
     Concat two string together.
     :raise: TypeError if A or B is not a string.
+    :return: concat result of a and b
     """
     if type(a) != str or type(b) != str:
         print("A and B must be strings.")
@@ -13,6 +14,7 @@ def remove_duplicate(a):
     """
     Check for duplicate character in string and remove.
     :raise: TypeError if A is not a string.
+    :return: string with no duplicate character
     """
     if type(a) != str:
         raise TypeError("A must be a string.")
@@ -29,6 +31,7 @@ def duplicate(a, round):
     """
     Duplicate string for specified round
     :raise: TypeError if A is not a string, or round is not a number
+    :return: string with specified round
     """
     return a * round
 
@@ -37,6 +40,8 @@ def find(a, char):
     """
     Find character in string and return index.a
     :raise: TypeError if a or char is not string.
+    :return: index of found character
+             -1 if not found
     """
     for i in range(len(a)):
         if a[i] == char:
